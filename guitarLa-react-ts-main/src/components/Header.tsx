16e47f8e12756a937a38900f95bc21a0448a395c
Jorge_R <alumno.656115@ies-azarquiel.es>
2026-06-22 19:18:39 +0200

    Añadir enlaces de despliegue a los README

--- guitarLa-react-ts-main/src/components/Header.tsx
+++ guitarLa-react-ts-main/src/components/Header.tsx
@@ -18,14 +18,14 @@ export default function Header({cart, removeFromCart, decreaseQuantity, increase
                 <div className="row justify-content-center justify-content-md-between">
                     <div className="col-8 col-md-3">
                         <a href="index.html">
-                            <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+                            <img className="img-fluid" src={`${import.meta.env.BASE_URL}img/logo.svg`} alt="imagen logo" />
                         </a>
                     </div>
                     <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                         <div 
                             className="carrito"
                         >
-                            <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+                            <img className="img-fluid" src={`${import.meta.env.BASE_URL}img/carrito.png`} alt="imagen carrito" />
 
                             <div id="carrito" className="bg-white p-3">
                                 {isEmpty ? (
@@ -48,7 +48,7 @@ export default function Header({cart, removeFromCart, decreaseQuantity, increase
                                                     <td>
                                                         <img 
                                                             className="img-fluid" 
-                                                            src={`/img/${guitar.image}.jpg`}
+                                                            src={`${import.meta.env.BASE_URL}img/${guitar.image}.jpg`}
                                                             alt="imagen guitarra" 
                                                         />
                                                     </td>
