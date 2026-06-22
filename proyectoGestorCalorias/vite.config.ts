4b00711e1990675deb344f35332eb93dcc7f8f58
Jorge_R <alumno.656115@ies-azarquiel.es>
2026-06-22 18:56:43 +0200

    Añadir enlaces de despliegue a los README

--- proyectoGestorCalorias/vite.config.ts
+++ proyectoGestorCalorias/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Proyectos/proyectoGestorCalorias/",
+  base: "/DIW/",
   server: {
     proxy: {
       '/api-usda': {
